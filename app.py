f2ceaff191a57dca03248407e4bca266123d9c54
Lucca <267645807+luccanarderocha-source@users.noreply.github.com>
2026-03-22 10:27:12 -0300

    Indicações das linhas de código do programa

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-print("Camculadora de Média de Consumo de Energia de Eletrodomésticos (CMCEE)")
+print("Camculadora de Média de Consumo de Energia de Eletrodomésticos (CMCEE)") # Título do programa.
 
-nomeDoAparelho = input("Qual o nome do aparelho? ")
-potenciaDoAparelho = float(input("Qual a potência do aparelho em watts(W)? "))
-tempoDeUso = float(input("Qual o tempo médio de uso em horas? "))
+nomeDoAparelho = input("Qual o nome do aparelho? ") # Entrada do nome do aparelho.
+potenciaDoAparelho = float(input("Qual a potência do aparelho em watts(W)? ")) # Entrada da potência do aparelho
+tempoDeUso = float(input("Qual o tempo médio de uso em horas? ")) # Entrada do tempo de uso.
 
-consumoMensal = (potenciaDoAparelho * tempoDeUso * 30) / 1000
+consumoMensal = (potenciaDoAparelho * tempoDeUso * 30) / 1000 # Processamento do consumo mensal do aparelho.
 
-print("O consumo mensal médio de seu(sua)", nomeDoAparelho, "é de:", consumoMensal, "KWh")
+print("O consumo mensal médio de seu(sua)", nomeDoAparelho, "é de:", consumoMensal, "KWh") # Saída do consumo mensal do aparelho.
